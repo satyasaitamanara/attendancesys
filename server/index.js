7e881539c928ac0a41320afa973bf51aceeb72b6
satyasaitamanara <tamanarasatyasai@gmail.com>
2025-07-03 18:00:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,10 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: "https://aknu-attendancesys.netlify.app", // ✅ Netlify URL
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
